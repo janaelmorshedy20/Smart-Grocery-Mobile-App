0127ac4e803e3fe7e66f5a2c192cd7eb7b8d248e
janaelmorshedy <janaelmorshedy20@gmail.com>
2024-12-30 00:25:07 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 // main.dart
 import 'package:flutter/material.dart';
-import 'package:smartgrocery/Login1.dart';
-import 'package:smartgrocery/WelcomeScreen.dart';
-import 'Signup.dart';
-import 'Login2.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
+import 'package:smartgrocery/login1.dart';
+import 'package:smartgrocery/welcomescreen.dart';
+import 'signup.dart';
+import 'login2.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure bindings are initialized
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(const MyApp());
 }
 
@@ -14,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
@@ -21,13 +27,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/welcome',
       routes: {
         '/signup': (context) => const SignUpScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/login2': (context) => const  LoginScreen2(),
-        '/welcome': (context) => const  WelcomeScreen(),
-
-        
-       
-        
+        // '/login': (context) => const LoginScreen(),
+        '/login2': (context) => const LoginScreen2(), 
+        '/welcome': (context) => const WelcomeScreen(),
       },
     );
   }
